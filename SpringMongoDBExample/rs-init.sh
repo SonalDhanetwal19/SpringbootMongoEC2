306009ec4fb08682ca0e8eb8b3b7738ab55d3aff
Sonal Dhanetwal Rai <sonal.dhanetwal.rai@external.toyota-europe.com>
2024-09-09 12:53:43 +0200

    updated docker-compose

---
+++ SpringMongoDBExample/rs-init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mongo <<EOF
+var config = {
+    "_id": "dbrs",
+    "version": 1,
+    "members": [
+        {
+            "_id": 1,
+            "host": "mongo_db1:27017",
+            "priority": 3
+        },
+        {
+            "_id": 2,
+            "host": "mongo_db2:27017",
+            "priority": 2
+        },
+        {
+            "_id": 3,
+            "host": "mongo_db3:27017",
+            "priority": 1
+        }
+    ]
+};
+rs.initiate(config, {force: true});
+rs.status();
+EOF
+
